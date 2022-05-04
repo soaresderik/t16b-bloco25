76d6221e97ca3dc83fc17368d4cd08b31bbf6d75
Andre Soares <andreferreira.sh@gmail.com>
2022-05-04 16:59:28 -0300

    stage commit heroku

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 app.get('/', (req, res) => {
   res.status(200).json({ ok: true, port: PORT })
+})
+
+app.get('/env', (req, res) => {
+  console.log({ NODE_ENV })
+  res.status(200).json({ ok: true, env: NODE_ENV })
 })
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
